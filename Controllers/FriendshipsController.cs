9481923c1f7818c1d0b8c0c0d11a7b6c361f316c
EmileGrenon <egrenon@bell.net>
2023-05-09 16:55:04 -0400

    main

--- Controllers/FriendshipsController.cs
+++ Controllers/FriendshipsController.cs
@@ -18,15 +18,25 @@ namespace ChatManager.Controllers
         [OnlineUsers.UserAccess]
         public PartialViewResult Friends(bool forceRefresh = false)
         {
-            if (forceRefresh || DB.Friendships.HasChanged) //Friends has changed methode a ajouter
+            if (forceRefresh || DB.Friendships.HasChanged || DB.Logins.HasChanged || DB.Users.HasChanged) //Friends has changed methode a ajouter
             {
                 return PartialView(DB.Friendships.ToList((int)Session["currentUserId"])); //retourner chaque ami sous forme de liste IENUMERABLE
             }
             return null;
         }
-        public void AddFriends()
+        [OnlineUsers.UserAccess]
+        public void FriendRequest(int id)
         {
-
+            var user = new Friendship();
+            var targetFriend = new Friendship();
+            user.Id = (int)Session["currentUserId"];
+            user.targetUserId = id;
+            user.friendshipStatus = 2;
+            targetFriend.Id = id;
+            targetFriend.targetUserId = (int)Session["currentUserId"];
+            targetFriend.friendshipStatus = 1;
+            DB.Friendships.Add(user);
+            DB.Friendships.Add(targetFriend);
         }
     }
 }
